4980f22f31004648423a37d87014db172c74f71e
miketommus <64033639+miketommus@users.noreply.github.com>
2024-05-03 00:33:46 -0700

    Updates

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-# EXAMPLE FATTY ACID ANALYSIS
+# **Example Fatty Acid Analyses Using R**
 
-This is an R script, written in Quarto markdown, that demonstrates how raw peak area data from a GCMS is processed into a useable form (concentration and/or proportion data); ready for plotting and statistical analysis. 
+There are two fatty acid analyses scripts contained herein. **Both of these scripts are used for demonstration purposes only.**
+
+1. "analysis_original.qmd" is a Quarto markdown script that demonstrates how to post-process GC/MS data of fatty acid methyl esters and begins plotting the data. This script is soon to be deprecated and will be replaced by the script described below.
+
+2. The other, "analysis_using_FATools.R", is an R script that demonstrates how to use the [FATools R package](https://www.github.com/miketommus/FATools) (currently in beta) to post-process GC/MS data of fatty acid methyl esters. The FATools package is currently under development and is subject to change. Thus, you shouldn't use it in any production code yet.
 
 The data in the /data directory comes from a real analysis of fatty acid methyl esters (FAME) with the exception of two dummy fatty acids (FA) added for testing purposes (18:1w9t & 20:1w5).
---
+++ analysis_using_FATools.R
@@ -0,0 +1,92 @@
+# ______ ___ _____           _
+# |  ___/ _ \_   _|         | |
+# | |_ / /_\ \| | ___   ___ | |___
+# |  _||  _  || |/ _ \ / _ \| / __|
+# | |  | | | || | (_) | (_) | \__ \
+# \_|  \_| |_/\_/\___/ \___/|_|___/
+
+# built with FATools v0.1 (beta)
+#==============================================================================
+# TITLE:
+#   Example Fatty Acid GC/MS Data Post-Processing
+
+# AUTHOR:
+#   miketommus
+
+# DESCRIPTION: This script contains an example analysis that shows how the use
+#   the FATools R Package [https://www.github.com/miketommus/FATools] to post-
+#   process raw data that results from the chromatographic analysis of fatty
+#   acid methyl esters (FAME).
+
+#==============================================================================
+# LOAD REQUIRED PACKAGES
+#==============================================================================
+library(FATools)    # post-processing of GCMS data
+library(readxl)     # reading in excel files
+library(dplyr)      # data manipulation
+library(magrittr)   # pipe operator
+
+#==============================================================================
+# SET VARIABLES FOR ANALYSIS
+#==============================================================================
+# Concentrations of external standards (ng/uL)
+ext_std_concs <- c(15, 50, 100, 250)
+
+#==============================================================================
+# DATA IMPORT
+#==============================================================================
+# Read in GC/MS peak area data
+file <- "path/to/your/file.csv"
+data <- read.csv(file)
+
+# Filter data to just compound data
+compound_data <- data[FATools::find_fa_name(colnames(data))]
+
+# Standardize fatty acid names in column names
+colnames(compound_data) %<>% convert_fa_name()
+
+# Source response factor mapping for instrument
+source("test_rf_map.R")
+test_rf_map$fa %<>% convert_fa_name()
+test_rf_map$ref_fa %<>% convert_fa_name()
+
+# Source proportion information for external standards
+source("test_ext_stds_props.R")
+test_nucheck_566c$fa %<>% convert_fa_name()
+test_nucheck_566c$prop %<>% as.numeric()
+
+# Remove fatty acids without positive IDs
+compound_data %<>% select(any_of(test_rf_map$fa))
+
+#==============================================================================
+# POST-PROCESSING
+#==============================================================================
+# Calculate GC/MS response factors
+response_factors <- calc_gc_response_factor(
+  data = compound_data[1:4,],
+  ext_std_concs = ext_std_concs,
+  ext_std_contents = test_nucheck_566c
+)
+
+# *** Above is not setting the col names of rf_table correctly.
+colnames(response_factors) <- c("fa", "rf")
+response_factors$rf %<>% as.numeric()
+
+# Convert Areas to Conc
+concentrations <- convert_area_to_conc(
+  data = compound_data,
+  rf_table = response_factors,
+  rf_map = test_rf_map
+)
+concentrations <- cbind(data[1:4], concentrations)
+
+# Convert Conc to Prop
+proportions <- convert_result_to_prop(
+  data = compound_data[-grep("19:0", colnames(compound_data))],
+  na.rm = TRUE
+)
+proportions <- cbind(data[1:4], proportions)
+
+#==============================================================================
+# HELPER FUNCTIONS
+#==============================================================================
